0aa3cbd1489e2386430d87cc1767178d2bdbd258
falend2802 <mohamadfalend15@gmail.com>
2022-08-18 21:42:28 +0700

    Updates

---
+++ precache-manifest.ec9249d41e61e86b5988b9bb6df90080.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "1fa447b0f78fbee48992",
+    "url": "/portofolio/static/css/main.12058fef.chunk.css"
+  },
+  {
+    "revision": "1fa447b0f78fbee48992",
+    "url": "/portofolio/static/js/main.1fa447b0.chunk.js"
+  },
+  {
+    "revision": "d17066d39ff52f97de39",
+    "url": "/portofolio/static/js/1.d17066d3.chunk.js"
+  },
+  {
+    "revision": "f9ab645a81c206d1f2da",
+    "url": "/portofolio/static/js/runtime~main.f9ab645a.js"
+  },
+  {
+    "revision": "961393917df5476ba0f238ab873dd36e",
+    "url": "/portofolio/static/media/CV.96139391.pdf"
+  },
+  {
+    "revision": "874985ed2f098f3c827df5d141b6787e",
+    "url": "/portofolio/static/media/me.874985ed.png"
+  },
+  {
+    "revision": "aaf23f25e9316d2f6a07e51cb8566191",
+    "url": "/portofolio/static/media/meAbout.aaf23f25.png"
+  },
+  {
+    "revision": "72169c637cbafe2f57f8a2de30126fa3",
+    "url": "/portofolio/static/media/portofolio1.72169c63.png"
+  },
+  {
+    "revision": "5a8f97ed23064e6a8e030579a7082ff7",
+    "url": "/portofolio/static/media/portofolio2.5a8f97ed.png"
+  },
+  {
+    "revision": "12d12ce9c1280174c23c108188b2cb5f",
+    "url": "/portofolio/static/media/portofolio3.12d12ce9.png"
+  },
+  {
+    "revision": "e508a8c9969984f6cba984f2bc34d976",
+    "url": "/portofolio/static/media/portofolio4.e508a8c9.png"
+  },
+  {
+    "revision": "7c41ac399e8ea30416389032fadf879b",
+    "url": "/portofolio/index.html"
+  }
+];
